9f83f2e80ac7922e13a17956df1e3863fbf20932
Jonathan Nguyen <jnguyen@modeln.com>
2016-03-13 23:25:31 -0700

    Added database creation. Will continue working on it

--- app/src/main/java/com/nguyeningcode/jnguyen7410/give_n_take/MainActivity.java
+++ app/src/main/java/com/nguyeningcode/jnguyen7410/give_n_take/MainActivity.java
@@ -42,9 +42,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        */
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
@@ -89,6 +89,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
--- app/src/main/java/com/nguyeningcode/jnguyen7410/give_n_take/NewRecordActivity.java
+++ app/src/main/java/com/nguyeningcode/jnguyen7410/give_n_take/NewRecordActivity.java
@@ -1,6 +1,7 @@
 package com.nguyeningcode.jnguyen7410.give_n_take;
 
 //import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,11 +13,22 @@ public class NewRecordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.i("NewRecord", "[onCreate] Successfully entered activity!");
+        Log.i("NewRecordActivity", "[onCreate] Successfully entered activity!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_record);
-        Log.i("NewRecord", "[onCreate] getSupportActionBar() = " + getSupportActionBar());
+        Log.i("NewRecordActivity", "[onCreate] getSupportActionBar() = " + getSupportActionBar());
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Log.i("NewRecordActivity", "[onCreate] Creating the database...");
+        SQLiteDatabase database = openOrCreateDatabase("Something", MODE_PRIVATE, null);
+
+        database.execSQL("CREATE TABLE IF NOT EXISTS Transaction" +
+                "(transaction_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "from TEXT, to TEXT, item TEXT, qty INTEGER, " +
+                "favorite TEXT NOT NULL CHECK (typeof(\"favorite\") = \"text\" AND \"favorite\" IN (\"TRUE\", \"FALSE\")), " +
+                "description TEXT, borrowed INTEGER, will_return INTEGER, category INTEGER, color TEXT");
+
+
 
     }
 
